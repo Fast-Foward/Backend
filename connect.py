f9708918ceffef844ba2fa467ed56f127673c392
anys34 <anyeseong34@gmail.com>
2023-11-30 13:54:54 +0000

    gitignore

--- connect.py
+++
@@ -1,9 +0,0 @@
-import pymysql
-
-conn = pymysql.connect(host='localhost', user='yeseong', password='qwer1234', db='fastfoward')
-cur = conn.cursor()
-cur.execute("SELECT * FROM Sports")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-conn.commit()
