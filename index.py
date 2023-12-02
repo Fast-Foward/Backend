0d033c1f67caa4477f998b05718793b36c789a46
anys34 <anyeseong34@gmail.com>
2023-12-02 14:02:46 +0000

    ADD :: cors

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 import RPi.GPIO as GPIO
 import time
 import pymysql
+from flask import Flask
+from flask_cors import CORS
 
 conn = pymysql.connect(host='localhost', user='yeseong', password='qwer1234', db='fastfoward')
 
@@ -8,3 +10,6 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 Sigpin = 8
 GPIO.setup(Sigpin, GPIO.IN)
+
+app = Flask(__name__)
+CORS(app)
